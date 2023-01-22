2112849cd98d03c2fd63e48924dc2268f2706aa7
Aparneesh <aprakashpatil27@ladueschools.net>
2023-01-22 03:17:54 +0000

    Changes

---
+++ 3.7.7/CSA_377_DataLab_StudentFiles/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args){
+        Cereal c = new Cereal("Apple Jacks", 4);
+        System.out.println(c.getName());
+        System.out.println(c.getProtein());
+    }
+    
+
+}
